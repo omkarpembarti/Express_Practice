115c623894f4e462ee2cdfe35c0e53c5aefc4174
omkar_pembarti <32337775+omkarpembarti@users.noreply.github.com>
2023-08-02 09:19:23 +0000

    Added external middleware -- Morgon

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const logger = require('./Logger');
-const app = express()
-const port = 3000
+const app = express();
+const morgon = require('morgan');
+const port = 3000;
 
 
 
-app.get('/',logger, (req, res) => {
+
+
+// app.get('/',logger, (req, res) => {
+//   res.send('Hello Omkar !')
+// })
+//  For above code we can also use
+//app.use(logger); //should be always at TOP
+
+app.use(morgon('tiny'));
+app.get('/', (req, res) => {
   res.send('Hello Omkar !')
 })
+
 app.get('/users', (req, res) => {
   res.status(200).send('All Users !')
 })
