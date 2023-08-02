5c488be0cc1ac7d63503d989b06a0abd5c7b0f0a
omkar_pembarti <32337775+omkarpembarti@users.noreply.github.com>
2023-08-02 07:00:56 +0000

    first random commit

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Omkar !')
+})
+app.get('/users', (req, res) => {
+  res.status(200).send('All Users !')
+})
+
+app.get('/user/:id', (req, res) => {
+  const {id} = req.params;
+  console.log(id);
+  res.status(200).send(`User -->${id}`);
 })
 
 app.listen(port, () => {
+  console.clear();
   console.log(`Example app listening on port ${port}`)
 })
